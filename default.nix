4957f4e5523112e9d2e62ae0cf44801f89d4bb2d
Sylvain Fankhauser <sephi@nibelheim.ch>
2025-09-17 09:13:31 +0200

    Fix build, move overlay out of systems

--- default.nix
+++ default.nix
@@ -1,9 +1,13 @@
-{ python3Packages }:
-python3Packages.buildPythonApplication rec {
+{ python, setuptools }:
+python.pkgs.buildPythonApplication rec {
   pname = "svgsprite";
   version = "0.1.1";
-  src = python3Packages.fetchPypi {
+  src = python.pkgs.fetchPypi {
     inherit pname version;
     sha256 = "sha256-EmMXWFw06k1R6XEId4IC5wqVdw7Rzmh/kgUC+bCBexw=";
   };
+
+  pyproject = true;
+
+  build-system = [ setuptools ];
 }
--- flake.nix
+++ flake.nix
@@ -1,21 +1,38 @@
 {
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    let
+      overlay = (final: prev: { svgsprite = final.python3.pkgs.callPackage ./. { }; });
+    in
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs {
           inherit system;
           overlays = [ overlay ];
         };
-        overlay = (final: prev: { svgsprite = final.callPackage ./. { }; });
-      in {
-        inherit overlay;
+      in
+      {
+        packages.default = pkgs.svgsprite;
 
-        defaultPackage = pkgs.svgsprite;
         devShell = pkgs.mkShell {
-          packages =
-            [ (pkgs.python3.withPackages (ps: [ ps.build ps.twine ])) ];
+          packages = [
+            (pkgs.python3.withPackages (ps: [
+              ps.build
+              ps.twine
+            ]))
+          ];
         };
-      });
+      }
+    )
+    // {
+      overlays.default = overlay;
+    };
 }
